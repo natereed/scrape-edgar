b4d3a423b1edef9377d5df940f263d25d7569ca0
reedndnb <reedn@dnb.com>
2015-07-20 15:51:59 -0500

    Refactor parse_html to use parse_text.

--- ScrapeEdgar/parsers/parser13ga.py
+++ ScrapeEdgar/parsers/parser13ga.py
@@ -21,7 +21,7 @@ class Parser13ga:
 
         #Alternative pattern
         # pat = re.compile("cusip (no|number|num)\.*:*\W*(\w{6}\W*\w{3})", re.IGNORECASE | re.MULTILINE)
-        # TBD: Implement fallback to this pattern if the first one doesn't match
+        # TBD: Implement fallback to this pattern if the first one doesn't match (use match.group 2)
 
         cusip = None
 
@@ -38,29 +38,5 @@ class Parser13ga:
         html_parser = HTML2TextParser()
         html_parser.feed(doc)
         text = html_parser.get_text()
-        address = parse_address(text)
 
-        print text
-
-        cusip_number = None
-
-        # CUSIP
-        pat = re.compile("cusip (no|number|num)\.*:*\W*(\w{6}\W*\w{3})", re.IGNORECASE | re.MULTILINE)
-
-        # We could use beautiful soup to find the pattern by applying it to all the table cells:
-        # cells = soup.findAll("td")
-        #        cells = filter(lambda x:pat.search(x.renderContents()), cells)
-        #       cusip_number = cells[0].text.replace(u'\xa0', " ")
-
-        match = pat.search(text)
-
-        if match:
-            print "---- MATCH!!!"
-            cusip_number = match.group(2)
-        else:
-            print "---- NO MATCH!!!"
-
-
-        results = {'cusip' : cusip_number, 'address' : address}
-        print results
-        return results
+        return self.parse_text(text)
