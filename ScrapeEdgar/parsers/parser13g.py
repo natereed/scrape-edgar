c689a07fed31b39b8ce11aa6458ff250d5625ad4
reedndnb <reedn@dnb.com>
2015-07-21 17:09:10 -0500

    Implement parsing for 8K-Ex.4.02 to extract issue name and CUSIP. Extracted BaseParser class.

--- ScrapeEdgar/parsers/parser13g.py
+++ ScrapeEdgar/parsers/parser13g.py
@@ -1,18 +1,10 @@
 import logging
 import re
 
-from ScrapeEdgar.parsers.html2textparser import HTML2TextParser
+from ScrapeEdgar.parsers.base_parser import BaseParser
 from ScrapeEdgar.parsers.address_text_parser import parse_address
 
-class Parser13g:
-    def parse(self, doc, content_type='text/html'):
-        if content_type == 'text/html':
-            return self.parse_html(doc)
-        elif content_type == 'text/plain':
-            return self.parse_text(doc)
-        else:
-            logging.warning("Unrecognized content type %s" % content_type)
-            return None
+class Parser13g(BaseParser):
 
     def parse_text(self, text):
         cusip_number = None
@@ -46,11 +38,5 @@ class Parser13g:
 
         return {'cusip': cusip_number, 'address': address, 'issue_name' : issue_name}
 
-    def parse_html(self, doc):
-         # Convert to text
-        html_parser = HTML2TextParser()
-        html_parser.feed(doc)
-        text = html_parser.get_text()
 
-        return self.parse_text(text)
 
--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
-python -m unittest ScrapeEdgar.parsers.parser13g_tests ScrapeEdgar.parsers.parser13ga_tests
+python -m unittest ScrapeEdgar.parsers.parser13g_tests\
+                   ScrapeEdgar.parsers.parser13ga_tests\
+                   ScrapeEdgar.parsers.parser8kex101_tests\
+                   ScrapeEdgar.parsers.parser8kex402_tests
+
+
+
 
 
