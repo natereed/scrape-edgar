85c948740de16af24d118a27b9a98185383ffde2
reedndnb <reedn@dnb.com>
2015-12-07 07:09:33 -0600

    PIC-344: Update Scrapyd IP address

--- schedule-job-on-aws.sh
+++ schedule-job-on-aws.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-curl http://54.215.229.61:6800/schedule.json -d project=ScrapeEdgar -d spider=edgar -d input_file=https://s3-us-west-1.amazonaws.com/edgar-crawler/sp500_companies_cleaned.csv
+curl http://ec2-54-183-74-84.us-west-1.compute.amazonaws.com:6800/schedule.json -d project=ScrapeEdgar -d spider=edgar -d input_file=https://s3-us-west-1.amazonaws.com/edgar-crawler/sp500_companies_cleaned.csv
 
